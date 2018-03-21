c7b8fdf47f008f1fe0912a7ec60de02aae3cb16e
arampersad11 <Amanda@Amanda-Rampersads-MacBook-Pro.local>
2018-03-20 20:23:50 -0400

    Spring boot application now runs
    Application now runs as spring boot app

--- src/main/java/configuration/Application.java
+++ src/main/java/configuration/Application.java
@@ -1,3 +1,5 @@
+package configuration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
